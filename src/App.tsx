5a9c1921bd285a6670858bdae00041fb34204486
aman mirza <aman mirza99>
2020-08-31 10:05:44 +0500

    todo list and todo items

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { TodoList } from './TodoList';
+
+const initialTodos: Todo[] = [
+  {
+    text: 'Walk the dog',
+    complete: false,
+  },
+  {
+    text: 'Write app',
+    complete: true,
+  },
+];
+
+function App() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  return <TodoList todos={todos} toggleTodo={toggleTodo} />;
+}
+
+export default App;
